3f037d43dc6744b17c01ff3d9668680142923638
Nimavein <michalzygiel00@gmail.com>
2021-06-06 17:01:41 +0200

    menu mobile create

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector(".menu-button");
 const burgerButton = document.querySelector(".menu-button-burger");
 const navbar = document.querySelector(".navbar");
+const navbarItems = document.querySelectorAll(".navbar-item");
 
 let showMenu = false;
 
@@ -11,11 +12,15 @@ function toggleMenu() {
     menuButton.classList.add("open");
     burgerButton.classList.add("open");
     navbar.classList.add("open");
+    navbarItems.forEach((item) => item.classList.add("open"));
+
     showMenu = true;
   } else {
     menuButton.classList.remove("open");
     burgerButton.classList.remove("open");
     navbar.classList.remove("open");
+    navbarItems.forEach((item) => item.classList.remove("open"));
+
     showMenu = false;
   }
 }
